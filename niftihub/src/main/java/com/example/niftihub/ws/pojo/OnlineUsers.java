6def6f4bca95fbc43e2f0c647e538b1470844dd5
ZephyrSky <936197658@qq.com>
2024-03-16 16:24:54 +0800

    ws相关

---
+++ niftihub/src/main/java/com/example/niftihub/ws/pojo/OnlineUsers.java
@@ -0,0 +1,20 @@
+package com.example.niftihub.ws.pojo;
+
+import jakarta.websocket.Session;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class OnlineUsers {
+    private static final Map<String, Session> onlineUsers = new ConcurrentHashMap<>();
+    public Map<String, Session> getOnlineUser(){
+        return onlineUsers;
+    }
+    public void addUser(String user,Session session){
+        onlineUsers.put(user,session);
+    }
+    //去除用户
+    public static void remove(String key){
+        onlineUsers.remove(key);
+    }
+}
